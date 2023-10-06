237c1f7bd2c3a30019b29cde8a463ad631ebb248
christophJJmueller <christoph_mueller91@web.de>
2023-10-06 15:30:26 +0200

    Recap Argc Argv

--- 03_ArrayPointer/3_2/MainArgs.cc
+++ 03_ArrayPointer/3_2/MainArgs.cc
@@ -2,17 +2,25 @@
 #include <iostream>
 
 // argc: argument count
+    //default: 1 (=exe)
 // argv: argument values
+    //**argv = *argv[] = 2D-Array
+        //1. Dimension: Anzahl Argumente = Anzahl Zeilen
+        //2. Dimension: so viele Einträge wie das jeweilige Argument lang ist
+    //Beispiel: gcc --version
+        //1. gcc = 3
+        //2. --version = 9
+
 int main(int argc, char **argv)
 {
     for (int i = 0; i < argc; ++i)
     {
-        std::cout << argv[i] << '\n';
+        std::cout << argv[i] << '\n'; //alle Argumente werden standardmäßig als String ausgegeben
     }
 
     if (argc > 1)
     {
-        int arg_value = std::atoi(argv[1]);
+        int arg_value = std::atoi(argv[1]); //https://en.cppreference.com/w/cpp/string/byte/atoi
         std::cout << arg_value << '\n';
     }
 
